8d3f2e5303de7a673edf235db5d8ed159370d7dc
trishi880988 <gauravyt8803@gmail.com>
2025-02-27 12:33:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # FIX ✅
 
 # MongoDB connection
 mongo_client = MongoClient(MONGO_URI)
